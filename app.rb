58afacf91ef9d6f7e4b2a473a0920f2b55c80cd1
James Holton <james_holton@yahoo.co.uk>
2019-09-12 11:38:18 +0100

    Added handling to stop empty url and returns success message

--- app.rb
+++ app.rb
@@ -7,6 +7,7 @@ class Bookmark_app < Sinatra::Base
   end
 
   get '/bookmarks' do
+    @screen_message = params[:message]
     bookmark_manager = Bookmark_Manager.new
     @bookmarks = bookmark_manager.return_bookmarks
     erb :bookmarks
@@ -20,9 +21,13 @@ class Bookmark_app < Sinatra::Base
   post '/createbookmark' do
     newurl = params[:newurl]
     title = params[:title]
-    bookmark_manager = Bookmark_Manager.new
-    bookmark_manager.add_bookmark(newurl, title)
-    @return_value_for_screen = 'url added successfully'
-    redirect '/bookmarks'
+    if newurl.length > 0
+      bookmark_manager = Bookmark_Manager.new
+      bookmark_manager.add_bookmark(newurl, title)
+      message = "Bookmark added successfully"
+    else
+      message = "No Bookmark entered"
+    end
+    redirect "/bookmarks?message=#{message}"
   end
 end
--- lib/bookmark_manager.rb
+++ lib/bookmark_manager.rb
@@ -3,15 +3,10 @@ require_relative './bookmark'
 
 class Bookmark_Manager
   def return_bookmarks
-    if ENV['ENVIRONMENT'] == 'test'
-      con = PG.connect :dbname => 'bookmark_manager_test'
-    else
-      con = PG.connect :dbname => 'bookmark_manager'
-    end
-
-    rs = con.exec 'SELECT id, title, url from bookmarks order by title'
     bookmarks = []
     begin
+      con = get_database_connection
+      rs = con.exec 'SELECT id, title, url from bookmarks order by title'
       rs.each do |row|
         bookmarks << Bookmark.new(id: row["id"], title: row["title"], url: row["url"])
       end
@@ -25,35 +20,22 @@ class Bookmark_Manager
   end
 
   def add_bookmark(bookmark, title = "default")
-    if ENV['ENVIRONMENT'] == 'test'
-      con = PG.connect :dbname => 'bookmark_manager_test'
-    else
-      con = PG.connect :dbname => 'bookmark_manager'
-    end
-
-    rs = con.exec "INSERT INTO bookmarks (title, url) VALUES('#{title}','#{bookmark}') RETURNING id, title, url;"
-    bookmark = Bookmark.new(id: rs[0]['id'], title: rs[0]['title'], url: rs[0]['url'])
-
     begin
+      con = get_database_connection
+      rs = con.exec "INSERT INTO bookmarks (title, url) VALUES('#{title}','#{bookmark}') RETURNING id, title, url;"
+      bookmark = Bookmark.new(id: rs[0]['id'], title: rs[0]['title'], url: rs[0]['url'])
     rescue PG::Error => e
       puts e.message
     ensure
       rs.clear if rs
       con.close if con
     end
-    bookmark
   end
 
   def delete_bookmark(id)
-    if ENV['ENVIRONMENT'] == 'test'
-      con = PG.connect :dbname => 'bookmark_manager_test'
-    else
-      con = PG.connect :dbname => 'bookmark_manager'
-    end
-
-    rs = con.exec "DELETE FROM bookmarks WHERE id = '#{id}'"
-
     begin
+      con = get_database_connection
+      rs = con.exec "DELETE FROM bookmarks WHERE id = '#{id}'"
     rescue PG::Error => e
       puts e.message
     ensure
@@ -62,4 +44,12 @@ class Bookmark_Manager
     end
   end
 
+  private
+  def get_database_connection
+    if ENV['ENVIRONMENT'] == 'test'
+      con = PG.connect :dbname => 'bookmark_manager_test'
+    else
+      con = PG.connect :dbname => 'bookmark_manager'
+    end
+  end
 end
